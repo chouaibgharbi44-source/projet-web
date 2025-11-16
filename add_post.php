c9866f8c5840729cbf0c255f2d81d0ef769cbc09
chouayb gharbi <chouaibgharbi44@gmail.com>
2025-11-16 21:09:22 +0100

    Add files via upload

---
+++ add_post.php
@@ -0,0 +1,12 @@
+<?php
+include "includes/db.php";
+
+$title = $_POST["title"];
+$content = $_POST["content"];
+
+$sql = "INSERT INTO posts (title, content) VALUES ('$title', '$content')";
+$conn->query($sql);
+
+header("Location: index.php");
+exit;
+?>
